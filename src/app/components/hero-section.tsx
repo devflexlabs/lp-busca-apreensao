38efd5f65c005e9e43fb717b68afc78414480f0f
Flex Labs <tecnologia@grupoflex.com.br>
2025-08-19 10:16:18 -0300

    style:  Ajuste de responsividade input

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -211,7 +211,7 @@ export default function HeroSection() {
                   </div>
 
                   {/* Parcelas e Vencimento */}
-                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  <div className="flex flex-col sm:flex-row gap-4 mx-auto justify-center">
                     <div>
                       <label className="block text-sm font-medium text-gray-200 mb-1">
                         Parcelas em atraso:
